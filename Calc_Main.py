4830a521536f2328a37471ce8714f2d410d824e7
Sentr-y <aslan.tas1@outlook.com>
2026-02-10 18:30:44 +0000

    Added GUI window with display

--- Calc_Main.py
+++ Calc_Main.py
@@ -1,5 +1,15 @@
 import tkinter as tk
 
-class Interface(tk.Tk):
+class Calculator(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("Lite Calculator")
+        self.current_expression = ""
+
+        # Display
+        self.display = tk.Entry(self, font=("Arial", 18), borderwidth=2, relief="ridge")
+        self.display.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+
+if __name__ == "__main__":
+    app = Calculator()
+    app.mainloop()
